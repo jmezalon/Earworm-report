c1702182d00f998f605955958dfe597a9060b457
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-03-22 11:25:12 -0400

    I just add some stuff to the home page so it would not be so blah

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,7 +3,10 @@ import React from 'react';
 export const Home = () => {
   return(
     <div className="Home1">
+
       <h1 id="homeh1">Earworm Report</h1>
+      <h4 id="homeh4">Welcome Maximax</h4>
+      <p>here you can add songs, search for songs, likes and leave a comment</p>
     </div>
   )
 }
